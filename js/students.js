8b8cec32926647ba246bc6f19ec66213bc27317c
OnlineTraining <OnlineTraining>
2021-06-02 12:15:41 +0700

    note

---
+++ js/students.js
@@ -0,0 +1,32 @@
+var listStudents = [
+    {
+        username:"huylg",
+        password: "huylam",
+        fullname: "Lâm Gia Huy",
+        email: "huylgps12908@fpt.edu.vn",
+        gender: "Nam",
+        birthday: "2001-06-10",
+        schoolfee: "15000",
+        marks: "10"
+    },
+    {
+        username:"haha",
+        password: "xyzw",
+        fullname: "Nguyễn Văn A",
+        email: "vana@fpt.edu.vn",
+        gender: "Nam",
+        birthday: "2005-06-23",
+        schoolfee: "1000",
+        marks: "9"
+    },
+    {
+        username:"girl",
+        password: "osad",
+        fullname: "Lê Thị B",
+        email: "thigai@fpt.edu.vn",
+        gender: "Nữ",
+        birthday: "2004-02-24",
+        schoolfee: "9000",
+        marks: "8"
+    }
+];
